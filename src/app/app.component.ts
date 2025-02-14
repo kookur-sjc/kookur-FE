7775b2e9bf56bdc6cbc0ad8c775e759b94902a6b
Justin Miranda <justin.ritheshmiranda@clarivate.com>
2025-02-14 10:24:44 +0530

    new ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { VideoComponent } from './video-handling/component/video/video.component';
 import { NavbarComponent } from './layout/navbar/navbar.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { HammerModule } from '@angular/platform-browser';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, VideoComponent, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, VideoComponent, NavbarComponent, FooterComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'kookur-FE';
+  showFooter = true;
+  constructor(private router: Router) {
+    this.router.events.subscribe(() => {
+      this.showFooter = !this.router.url.includes('/player');
+    });
+  }
 }
