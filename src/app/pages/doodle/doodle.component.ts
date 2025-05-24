b78bff8cb85c34d0adc114b1bd65192eafca3355
Justin Rithesh Miranda <Justin.RitheshMiranda@Clarivate.com>
2025-05-24 19:24:34 +0530

    cricket 2

--- src/app/pages/doodle/doodle.component.ts
+++ src/app/pages/doodle/doodle.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-doodle',
@@ -9,16 +10,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './doodle.component.scss'
 })
 export class DoodleComponent implements OnInit {
-gamePath = 'assets/doodle-cricket/index.html';
+  sanitizedGamePath: SafeResourceUrl;
   
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private sanitizer: DomSanitizer
+  ) {
+    // Default path
+    const defaultPath = '/assets/doodle-cricket/index.html';
+    this.sanitizedGamePath = this.sanitizer.bypassSecurityTrustResourceUrl(defaultPath);
+  }
+  
   ngOnInit() {
-    // Check if the file exists
-    this.http.get(this.gamePath, { responseType: 'text' })
+    // First try with absolute path
+    const path = '/assets/doodle-cricket/index.html';
+    
+    // Check if file exists with a HEAD request (more efficient)
+    this.http.head(path)
       .subscribe(
-        () => console.log('Game HTML found!'),
-        error => console.error('Game HTML not found!', error)
+        () => {
+          console.log('Game found at:', path);
+          this.sanitizedGamePath = this.sanitizer.bypassSecurityTrustResourceUrl(path);
+        },
+        error => {
+          console.warn('Failed to load game from primary path, trying alternative paths');
+          this.tryAlternativePaths();
+        }
       );
   }
-
+  
+  private tryAlternativePaths() {
+    // Try alternative paths in sequence
+    const paths = [
+      './assets/doodle-cricket/index.html',
+      '../assets/doodle-cricket/index.html',
+      '/assets/doodle-cricket/Cricket-Doodle-Game/index.html',
+      './assets/doodle-cricket/Cricket-Doodle-Game/index.html'
+    ];
+    
+    let pathsChecked = 0;
+    
+    for (const path of paths) {
+      this.http.head(path)
+        .subscribe(
+          () => {
+            console.log('Game found at alternative path:', path);
+            this.sanitizedGamePath = this.sanitizer.bypassSecurityTrustResourceUrl(path);
+          },
+          error => {
+            pathsChecked++;
+            if (pathsChecked === paths.length) {
+              console.error('Game could not be found at any expected location');
+            }
+          }
+        );
+    }
+  }
 }
