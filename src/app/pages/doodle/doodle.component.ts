8db30ee3de5fce9bdf43823138af59a761ce6cdf
Justin Rithesh Miranda <Justin.RitheshMiranda@Clarivate.com>
2025-05-24 19:12:17 +0530

    game change

--- src/app/pages/doodle/doodle.component.ts
+++ src/app/pages/doodle/doodle.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-doodle',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './doodle.component.html',
   styleUrl: './doodle.component.scss'
 })
-export class DoodleComponent {
+export class DoodleComponent implements OnInit {
+gamePath = 'assets/doodle-cricket/index.html';
+  
+  constructor(private http: HttpClient) {}
+  ngOnInit() {
+    // Check if the file exists
+    this.http.get(this.gamePath, { responseType: 'text' })
+      .subscribe(
+        () => console.log('Game HTML found!'),
+        error => console.error('Game HTML not found!', error)
+      );
+  }
 
 }
